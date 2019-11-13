53618fa9a3a5cbc2dc893ae93a638757a560314a
Race <raceacheson@gmail.com>
2019-11-13 14:37:52 -0700

    first commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <template v-slot:lead>{{ currentQuestion.question}}</template>
+
+      <hr class="my-4" />
+
+      <b-list-group>
+        <b-list-group-item
+          @click="selectAnswer(index)"
+          v-for="(answer, index) in shuffledAnswers"
+          :key="answer"
+          @click.prevent="selectAnswer(index)"
+          :class="[
+          !answered && selectedIndex === index ? 'selected' :
+          answered && correctIndex === index ? 'correct' : 
+          answered && selectedIndex === index && correctIndex !== index ? 'incorrect' : ''
+          ]"
+        >{{answer}}</b-list-group-item>
+      </b-list-group>
+
+      <b-button 
+      variant="primary"
+      @click="submitAnswer"
+      :disabled="selectedIndex === null || answered"
+      >Submit
+      </b-button>
+      <b-button @click="next" variant="success">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+import _ from "lodash";
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+    increment: Function
+  },
+  data: function() {
+    return {
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
+    };
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
+  },
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null;
+        this.answered = false
+        this.shuffleAnswers();
+      }
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+    },
+    shuffleAnswers() {
+      let answers = [
+        ...this.currentQuestion.incorrect_answers,
+        this.currentQuestion.correct_answer
+      ];
+      this.shuffledAnswers = _.shuffle(answers);
+       this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+    },
+    submitAnswer(){
+        let isCorrect = false;
+        if ( this.selectedIndex === this.correctIndex) { 
+            isCorrect = true
+        }
+        this.answered = true
+        this.increment(isCorrect)
+
+
+
+    }
+  },
+  mounted() {
+    this.shuffleAnswers();
+
+  }
+};
+</script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+
+.list-group-item {
+  cursor: pointer;
+}
+.list-group-item:hover {
+  background: rgb(204, 204, 204);
+}
+
+.selected {
+  background: lightblue;
+}
+
+.correct {
+  background: lightgreen;
+}
+.incorrect {
+  background: pink;
+}
+
+.btn {
+  margin: 5px;
+}
+</style>
